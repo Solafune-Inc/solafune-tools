60ceaff98acdd1f9c77d658c8c767e1c61c9c726
Lanang Afkaar <afkaar2012@live.com>
2025-03-09 05:59:12 +0700

    [UPDATE] update test unit for the new developed function also update the documentation

--- docs/metrics_toolset.md
+++ docs/metrics_toolset.md
@@ -8,7 +8,7 @@ In this repository we also add metrics toolset to accomodate user to be able use
 
 This function computes the Panoptic Qualities (PQ) score which leveraged Intersection Over Union (IoU) for a given set of predictions and ground truth polygons. The PQ score is a metric used to evaluate the quality of panoptic segmentation models. The PQ score is computed as the sum of the PQ scores for each class in the dataset. The PQ score for a class is computed as the sum of the true positive, false positive, and false negative values for that class. The PQ score is then normalized by the sum of the true positive and false negative values for that class. The PQ score is a value between 0 and 1, with 1 being the best possible score.
 
-https://arxiv.org/abs/1801.00868
+[Paper Implementation](https://arxiv.org/abs/1801.00868)
 
 #### Authors information
 
@@ -30,7 +30,7 @@ polygon4 = Polygon([(2, 2), (3, 4), (4, 4), (5, 2), (3, 1)])
 ground_truth_polygons = [polygon1, polygon2]
 prediction_polygons = [polygon3, polygon4]
 
-pq, sq, rq = PQ.compute_pq(ground_truth_polygons, prediction_polygons)
+pq, sq, rq = PQ.compute_pq(ground_truth_polygons, prediction_polygons, iou_threshold=0.5)
 
 print("PQ: ", pq)
 print("SQ: ", sq)
@@ -41,6 +41,7 @@ print("RQ: ", rq)
 
 - ground_truth_polygons: List of polygons representing the ground truth segmentation.
 - prediction_polygons: List of polygons representing the predicted segmentation.
+- iou_threshold: Threshold for the Intersection over Union. default is 0.5
 
 polygons is shapely.geometry.Polygon object
 https://shapely.readthedocs.io/en/stable/
@@ -75,7 +76,7 @@ polygon4 = Polygon([(2, 2), (3, 4), (4, 4), (5, 2), (3, 1)])
 ground_truth_polygons = [polygon1, polygon2]
 prediction_polygons = [polygon3, polygon4]
 
-f1, precision, recall = F1_score.compute_f1(ground_truth_polygons, prediction_polygons)
+f1, precision, recall = F1_score.compute_f1(ground_truth_polygons, prediction_polygons, iou_threshold=0.5)
 
 print("F1: ", f1)
 print("Precision: ", precision)
@@ -86,6 +87,7 @@ print("Recall: ", recall)
 
 - ground_truth_polygons: List of polygons representing the ground truth segmentation.
 - prediction_polygons: List of polygons representing the predicted segmentation.
+- iou_threshold: Threshold for the Intersection over Union. default is 0.5
 
 polygons is shapely.geometry.Polygon object
 https://shapely.readthedocs.io/en/stable/
@@ -95,6 +97,47 @@ https://shapely.readthedocs.io/en/stable/
 - f1: F1 score
 - precision: Precision value
 - recall: Recall value
+
+### Mean Average Precision Function
+This function computes the Mean Average Precision (mAP) score, which is a common metric used to evaluate the performance of object detection models. The mAP score is calculated by averaging the precision scores at different recall levels. It provides a single value that summarizes the precision-recall trade-off for a given set of predictions and ground truth annotations. The mAP score ranges from 0 to 1, with 1 indicating perfect precision and recall.
+
+#### Authors Information
+
+Author: Lanang Afkaar \
+Solafune username: Fulankun1412
+
+#### Getting Started with mAP score
+
+```python
+from shapely.geometry import Polygon
+from solafune_tools.metrics import IOUBasedMetrics
+mAP = IOUBasedMetrics()
+
+polygon1 = Polygon([(1, 2), (2, 4), (3, 1)])
+polygon2 = Polygon([(0, 0), (1, 3), (2, 2), (3, 0)])
+polygon3 = Polygon([(5, 5), (6, 6), (7, 5), (8, 4), (5, 3)])
+polygon4 = Polygon([(2, 2), (3, 4), (4, 4), (5, 2), (3, 1)])
+
+ground_truth_polygons = [polygon1, polygon2]
+prediction_polygons = [polygon3, polygon4]
+
+map_score = mAP.compute_map(ground_truth_polygons, prediction_polygons, iou_threshold=0.5)
+
+print("mAP: ", map_score)
+```
+
+#### Input
+
+- ground_truth_polygons: List of polygons representing the ground truth segmentation.
+- prediction_polygons: List of polygons representing the predicted segmentation.
+- iou_threshold: Threshold for the Intersection over Union. default is [0.5, 0.95, 0.05]
+
+polygons is shapely.geometry.Polygon object
+https://shapely.readthedocs.io/en/stable/
+
+#### Output
+
+- map_score: Mean Average Precision score
 
 ## Pixel-Based Metrics
 
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,14 +1,71 @@
 from shapely.geometry import Polygon
-from solafune_tools.metrics import IOUBasedMetrics, PixelBasedMetrics
+from solafune_tools.metrics import IOUBasedMetrics, PixelBasedMetrics, bbox_to_polygon
 
 # ---------------------> The test below is for the IOUBasedMetrics class
 
+## Test bbox_to_polygon
+def test_bbox_to_polygon():
+    bbox = [0, 0, 1, 1]
+    assert bbox_to_polygon(bbox) == Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])
+
+## Test for the IOUBasedMetrics of Object Detection Task
+def test_get_iou():
+    polygon1 = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
+    polygon2 = Polygon([(0, 0), (0, 1), (2, 1), (2, 0)])
+    assert IOUBasedMetrics().getIOU(polygon1, polygon2)
+
+def test_iou_based_compute_map_bbox():
+    gt_bboxes = [[0, 0, 1, 1], [1, 1, 1, 1], [2, 2, 1, 1]]
+    pred_bboxes = [[0, 0, 1, 1], [1, 1, 1, 1], [2, 2, 1, 1]]
+    gt_polygons = [bbox_to_polygon(bbox) for bbox in gt_bboxes]
+    pred_polygons = [bbox_to_polygon(bbox) for bbox in pred_bboxes]
+    
+    MAP = IOUBasedMetrics()
+    map_score = MAP.compute_map(gt_polygons, pred_polygons)
+    assert map_score == 0.6666666666666666
+
+def test_iou_based_compute_map_bbox_same_score_with_dif_order():
+    gt_bboxes = [[0, 0, 1, 1], [1, 1, 1, 1], [2, 2, 1, 1]]
+    pred_bboxes = [[0, 0, 1, 1], [1, 1, 1, 1], [2, 2, 1, 1]]
+    gt_order_1 = [1, 0, 2]
+    pred_order_1 = [0, 1, 2]
+    gt_order_2 = [0, 1, 2]
+    pred_order_2 = [1, 0, 2]
+    gt_polygons = [bbox_to_polygon(gt_bboxes[i]) for i in gt_order_1]
+    pred_polygons = [bbox_to_polygon(pred_bboxes[i]) for i in pred_order_1]
+    MAP = IOUBasedMetrics()
+    map_score_1 = MAP.compute_map(gt_polygons, pred_polygons)
+    gt_polygons = [bbox_to_polygon(gt_bboxes[i]) for i in gt_order_2]
+    pred_polygons = [bbox_to_polygon(pred_bboxes[i]) for i in pred_order_2]
+    map_score_2 = MAP.compute_map(gt_polygons, pred_polygons)
+    assert map_score_1 == map_score_2
+
+def test_iou_based_compute_map_bbox_zero_score():
+    gt_bboxes = [[0, 0, 1, 1], [1, 1, 1, 1], [2, 2, 1, 1]]
+    pred_bboxes = [[3, 3, 1, 1], [4, 4, 1, 1], [5, 5, 1, 1]]
+    gt_polygons = [bbox_to_polygon(bbox) for bbox in gt_bboxes]
+    pred_polygons = [bbox_to_polygon(bbox) for bbox in pred_bboxes]
+    MAP = IOUBasedMetrics()
+    map_score = MAP.compute_map(gt_polygons, pred_polygons)
+    assert map_score == 0
+
+def test_iou_based_compute_map_bbox_perfect_score():
+    gt_bboxes = [[0, 0, 1, 1], [1, 1, 1, 1], [2, 2, 1, 1]]
+    pred_bboxes = [[0, 0, 1, 1], [1, 1, 1, 1], [2, 2, 1, 1]]
+    gt_polygons = [bbox_to_polygon(bbox) for bbox in gt_bboxes]
+    pred_polygons = [bbox_to_polygon(bbox) for bbox in pred_bboxes]
+    MAP = IOUBasedMetrics()
+    map_score = MAP.compute_map(gt_polygons, pred_polygons, iou_thresholds=0.5)
+    assert map_score == 1
+
+## Test for the IOUBasedMetrics of Segmentation Task
 def test_get_iou():
     polygon1 = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
     polygon2 = Polygon([(0, 0), (0, 1), (2, 1), (2, 0)])
     assert IOUBasedMetrics().getIOU(polygon1, polygon2) == 0.5
 
-def test_iou_based_compute_pq():
+### Test for the IOUBasedMetrics of Segmentation Task Compute PQ
+def test_iou_based_compute_pq_segmentation():
     polygon1 = Polygon([(1, 2), (2, 4), (3, 1)])
     polygon2 = Polygon([(0, 0), (1, 3), (2, 2), (3, 0)])
     polygon3 = Polygon([(5, 5), (6, 6), (7, 5), (8, 4), (5, 3)])
@@ -27,7 +84,7 @@ def test_iou_based_compute_pq():
     assert sq == 1
     assert round(rq,1) == 0.8
     
-def test_iou_based_compute_pq_same_score_with_dif_order():
+def test_iou_based_compute_pq_segmentation_same_score_with_dif_order():
     true_polygons = [
         Polygon([(5, 5), (6, 6), (7, 5), (8, 4), (5, 3), (5, 5)]),
         Polygon([(4, 4), (5, 6), (7, 7), (8, 5), (7, 4), (4, 4)]),
@@ -50,9 +107,7 @@ def test_iou_based_compute_pq_same_score_with_dif_order():
     assert sq1 == sq2
     assert rq1 == rq2
 
-from shapely.geometry import Polygon
-
-def test_compute_pq_zero_score():
+def test_compute_pq_segmentation_zero_score():
     true_polygons = [
         Polygon([(5, 5), (6, 6), (7, 5), (8, 4), (5, 3), (5, 5)]),
         Polygon([(4, 4), (5, 6), (7, 7), (8, 5), (7, 4), (4, 4)]),
@@ -69,7 +124,7 @@ def test_compute_pq_zero_score():
     assert sq == 0
     assert rq == 0
 
-def test_iou_based_compute_pq_perfect_score():
+def test_iou_based_compute_pq_segmentation_perfect_score():
     true_polygons = [
         Polygon([(5, 5), (6, 6), (7, 5), (8, 4), (5, 3), (5, 5)]),
         Polygon([(4, 4), (5, 6), (7, 7), (8, 5), (7, 4), (4, 4)]),
@@ -86,7 +141,8 @@ def test_iou_based_compute_pq_perfect_score():
     assert sq == 1
     assert rq == 1
 
-def test_iou_based_f1_score():
+### Test for the IOUBasedMetrics of Segmentation F1 Score
+def test_iou_based_f1_score_segmentation():
     polygon1 = Polygon([(1, 2), (2, 4), (3, 1)])
     polygon2 = Polygon([(0, 0), (1, 3), (2, 2), (3, 0)])
     polygon3 = Polygon([(5, 5), (6, 6), (7, 5), (8, 4), (5, 3)])
@@ -105,7 +161,7 @@ def test_iou_based_f1_score():
     assert precision == 0.75
     assert round(recall,1) == 0.8
 
-def test_iou_based_f1_score_same_score_with_dif_order():
+def test_iou_based_f1_score_segmentation_same_score_with_dif_order():
     true_polygons = [
         Polygon([(5, 5), (6, 6), (7, 5), (8, 4), (5, 3), (5, 5)]),
         Polygon([(4, 4), (5, 6), (7, 7), (8, 5), (7, 4), (4, 4)]),
@@ -128,7 +184,7 @@ def test_iou_based_f1_score_same_score_with_dif_order():
     assert precision_1 == precision_2
     assert recall_1 == recall_2
 
-def test_iou_based_f1_score_zero_score():
+def test_iou_based_f1_score_segmentation_zero_score():
     true_polygons = [
         Polygon([(5, 5), (6, 6), (7, 5), (8, 4), (5, 3), (5, 5)]),
         Polygon([(4, 4), (5, 6), (7, 7), (8, 5), (7, 4), (4, 4)]),
@@ -145,7 +201,7 @@ def test_iou_based_f1_score_zero_score():
     assert precision == 0
     assert recall == 0
 
-def test_iou_based_f1_score_perfect_score():
+def test_iou_based_f1_score_segmentation_perfect_score():
     true_polygons = [
         Polygon([(5, 5), (6, 6), (7, 5), (8, 4), (5, 3), (5, 5)]),
         Polygon([(4, 4), (5, 6), (7, 7), (8, 5), (7, 4), (4, 4)]),
@@ -161,6 +217,74 @@ def test_iou_based_f1_score_perfect_score():
     assert f1 == 1
     assert precision == 1
     assert recall == 1
+
+### Test for the IOUBasedMetrics of Segmentation Mean Average Precision
+def test_iou_based_compute_map_segmentation():
+    polygon1 = Polygon([(1, 2), (2, 4), (3, 1)])
+    polygon2 = Polygon([(0, 0), (1, 3), (2, 2), (3, 0)])
+    polygon3 = Polygon([(5, 5), (6, 6), (7, 5), (8, 4), (5, 3)])
+    polygon4 = Polygon([(2, 2), (3, 4), (4, 4), (5, 2), (3, 1)])
+    polygon5 = Polygon([(4, 4), (5, 6), (7, 7), (8, 5), (7, 4)])
+    polygon6 = Polygon([(1, 1), (2, 3), (3, 3), (2, 1)])
+    polygon7 = Polygon([(3, 3), (4, 5), (6, 5), (7, 3), (5, 2)])
+    
+    true_polygons = [polygon1, polygon3, polygon5, polygon7]
+    pred_polygons = [polygon1, polygon2, polygon3, polygon7]
+    
+    MAP = IOUBasedMetrics()
+    map_score = MAP.compute_map(true_polygons, pred_polygons)
+    assert round(map_score,1) == 0.5
+
+def test_iou_based_compute_map_segmentation_same_score_with_dif_order():
+    true_polygons = [
+        Polygon([(5, 5), (6, 6), (7, 5), (8, 4), (5, 3), (5, 5)]),
+        Polygon([(4, 4), (5, 6), (7, 7), (8, 5), (7, 4), (4, 4)]),
+        Polygon([(3, 3), (4, 5), (6, 5), (7, 3), (5, 2), (3, 3)]),
+    ]
+    pred_polygons = [
+        Polygon([(7, -3), (8, -2), (9, -3), (10, -4), (7, -5), (7, -3)]),
+        Polygon([(9, 8), (10, 10), (12, 11), (13, 9), (12, 8), (9, 8)]),
+        Polygon([(4, 4), (5, 6), (7, 6), (8, 4), (6, 3), (4, 4)]),
+    ]
+    true_order_1 = [1, 0, 2]
+    pred_order_1 = [0, 1, 2]
+    true_order_2 = [0, 1, 2]
+    pred_order_2 = [1, 0, 2]
+    MAP = IOUBasedMetrics()
+    map_score_1 = MAP.compute_map([true_polygons[i] for i in true_order_1], [pred_polygons[i] for i in pred_order_1])
+    map_score_2 = MAP.compute_map([true_polygons[i] for i in true_order_2], [pred_polygons[i] for i in pred_order_2])
+    
+    assert map_score_1 == map_score_2
+
+def test_iou_based_compute_map_segmentation_zero_score():
+    true_polygons = [
+        Polygon([(5, 5), (6, 6), (7, 5), (8, 4), (5, 3), (5, 5)]),
+        Polygon([(4, 4), (5, 6), (7, 7), (8, 5), (7, 4), (4, 4)]),
+        Polygon([(3, 3), (4, 5), (6, 5), (7, 3), (5, 2), (3, 3)]),
+    ]
+    pred_polygons = [
+        Polygon([(20, 20), (21, 21), (22, 20), (23, 19), (20, 18), (20, 20)]),
+        Polygon([(30, 30), (31, 32), (33, 33), (34, 31), (33, 30), (30, 30)]),
+        Polygon([(40, 40), (41, 42), (43, 42), (44, 40), (42, 39), (40, 40)]),
+    ]
+    MAP = IOUBasedMetrics()
+    map_score = MAP.compute_map(true_polygons, pred_polygons)
+    assert map_score == 0
+
+def test_iou_based_compute_map_segmentation_perfect_score():
+    true_polygons = [
+        Polygon([(5, 5), (6, 6), (7, 5), (8, 4), (5, 3), (5, 5)]),
+        Polygon([(4, 4), (5, 6), (7, 7), (8, 5), (7, 4), (4, 4)]),
+        Polygon([(3, 3), (4, 5), (6, 5), (7, 3), (5, 2), (3, 3)]),
+    ]
+    pred_polygons = [
+        Polygon([(5, 5), (6, 6), (7, 5), (8, 4), (5, 3), (5, 5)]),
+        Polygon([(4, 4), (5, 6), (7, 7), (8, 5), (7, 4), (4, 4)]),
+        Polygon([(3, 3), (4, 5), (6, 5), (7, 3), (5, 2), (3, 3)]),
+    ]
+    MAP = IOUBasedMetrics()
+    map_score = MAP.compute_map(true_polygons, pred_polygons, iou_thresholds=0.5)
+    assert map_score == 1
 
 # ---------------------> Below are the tests for PixelBasedMetrics
 
